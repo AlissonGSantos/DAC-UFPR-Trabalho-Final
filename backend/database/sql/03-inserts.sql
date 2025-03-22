40d7e281d87bc0036f3c888bf12f6fe118f54742
salgado2004 <leosalgado2004@gmail.com>
2025-03-22 19:23:04 -0300

    chore: database configuration

---
+++ backend/database/sql/03-inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO emiratads_funcionario.funcionario (cpf, nome, email, telefone) VALUES
+    ('90769281001', 'Funcionario 1', 'func_pre@gmail.com', '1234567890'),
+    ('23456789012', 'Funcionario 2', 'func2@example.com', '2345678901'),
+    ('34567890123', 'Funcionario 3', 'func3@example.com', '3456789012'),
+    ('45678901234', 'Funcionario 4', 'func4@example.com', '4567890123'),
+    ('56789012345', 'Funcionario 5', 'func5@example.com', '5678901234');
