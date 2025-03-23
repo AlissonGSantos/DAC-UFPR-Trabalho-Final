54a9b55f8ebf2b988e6aeed8809dd4f833987dcb
salgado2004 <leosalgado2004@gmail.com>
2025-03-23 12:29:11 -0300

    chore: cliente-service dependencies adition

--- backend/database/sql/02-create-tables.sql
+++ backend/database/sql/02-create-tables.sql
@@ -13,4 +13,44 @@ CREATE TABLE emiratads_funcionario.funcionario (
     email varchar(30) NOT NULL,
     telefone varchar(14),
     CONSTRAINT funcionario_pkey PRIMARY KEY (codigo)
-)
+);
+
+CREATE SEQUENCE emiratads_cliente.cliente_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+CREATE SEQUENCE emiratads_cliente.endereco_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+CREATE TABLE emiratads_cliente.endereco (
+    codigo serial4 NOT NULL,
+    cep varchar(8) NOT NULL,
+    uf varchar(2) NOT NULL,
+    cidade varchar(30) NOT NULL,
+    bairro varchar(30) NOT NULL,
+    rua varchar(30) NOT NULL,
+    numero varchar(10) NOT NULL,
+    complemento varchar(10),
+    CONSTRAINT endereco_pkey PRIMARY KEY (codigo)
+);
+
+CREATE TABLE emiratads_cliente.cliente (
+    codigo serial4 NOT NULL,
+    cpf varchar(11) NOT NULL,
+    nome varchar(50) NOT NULL,
+    email varchar(30) NOT NULL,
+    saldo_milhas numeric(10, 2) NOT NULL,
+    endereco_codigo int4 NOT NULL,
+    CONSTRAINT cliente_pkey PRIMARY KEY (codigo),
+    CONSTRAINT cliente_unique_cpf UNIQUE (cpf),
+    CONSTRAINT cliente_endereco_fk FOREIGN KEY (endereco_codigo) REFERENCES emiratads_cliente.endereco(codigo)
+);
--- backend/database/sql/03-inserts.sql
+++ backend/database/sql/03-inserts.sql
@@ -1,6 +1,23 @@
+-- Inserts para a tabela funcionario
 INSERT INTO emiratads_funcionario.funcionario (cpf, nome, email, telefone) VALUES
     ('90769281001', 'Funcionario 1', 'func_pre@gmail.com', '1234567890'),
     ('23456789012', 'Funcionario 2', 'func2@example.com', '2345678901'),
     ('34567890123', 'Funcionario 3', 'func3@example.com', '3456789012'),
     ('45678901234', 'Funcionario 4', 'func4@example.com', '4567890123'),
     ('56789012345', 'Funcionario 5', 'func5@example.com', '5678901234');
+
+-- Inserts para a tabela endereco
+INSERT INTO emiratads_cliente.endereco (cep, uf, cidade, bairro, rua, numero, complemento) VALUES
+    ('80000000', 'PR', 'Curitiba', 'Centro', 'Rua 1', '123', 'Apto 101'),
+    ('80010000', 'PR', 'Curitiba', 'Batel', 'Rua 2', '456', 'Apto 202'),
+    ('80020000', 'PR', 'Curitiba', 'Água Verde', 'Rua 3', '789', 'Casa 1'),
+    ('80030000', 'PR', 'Curitiba', 'Juvevê', 'Rua 4', '101', 'Casa 2'),
+    ('80040000', 'PR', 'Curitiba', 'Cabral', 'Rua 5', '202', 'Apto 303');
+
+-- Inserts para a tabela cliente
+INSERT INTO emiratads_cliente.cliente (cpf, nome, email, saldo_milhas, endereco_codigo) VALUES
+    ('12345678901', 'Cliente 1', 'cliente1@example.com', 0.0, 1),
+    ('23456789012', 'Cliente 2', 'cliente2@example.com', 0.0, 2),
+    ('34567890123', 'Cliente 3', 'cliente3@example.com', 0.0, 3),
+    ('45678901234', 'Cliente 4', 'cliente4@example.com', 0.0, 4),
+    ('56789012345', 'Cliente 5', 'cliente5@example.com', 0.0, 5);
