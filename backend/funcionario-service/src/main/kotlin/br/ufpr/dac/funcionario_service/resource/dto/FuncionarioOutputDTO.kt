9fe645801e1edcbe80a1efd99043a5574efb0966
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-03-23 20:10:00 -0300

    #25 fix: codigo can be null

--- backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/dto/FuncionarioOutputDTO.kt
+++ backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/dto/FuncionarioOutputDTO.kt
@@ -3,7 +3,7 @@ package br.ufpr.dac.funcionario_service.resource.dto
 import br.ufpr.dac.funcionario_service.domain.Funcionario
 
 data class FuncionarioOutputDTO(
-    val codigo: Long,
+    val codigo: Long? = null,
     val cpf: String,
     val nome: String,
     val email: String,
