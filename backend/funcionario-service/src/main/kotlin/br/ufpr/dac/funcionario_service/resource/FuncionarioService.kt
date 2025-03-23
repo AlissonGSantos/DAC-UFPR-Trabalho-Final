44174ce2312ec6958eb29c54a778a54573026735
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-03-23 17:24:54 -0300

    #25 fix: add FuncionarioService file

---
+++ backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/FuncionarioService.kt
@@ -0,0 +1,4 @@
+package br.ufpr.dac.funcionario_service.resource
+
+class FuncionarioService {
+}
--- backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/FuncionarioService/kt.java
+++
@@ -1,4 +0,0 @@
-package br.ufpr.dac.funcionario_service.resource.FuncionarioService;
-
-public class kt {
-}
