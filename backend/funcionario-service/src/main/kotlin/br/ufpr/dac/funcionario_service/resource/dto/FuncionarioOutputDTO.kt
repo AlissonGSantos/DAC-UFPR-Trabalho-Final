8611e133d3656853ed7c73d779e2f8e38a7c97b0
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-03-23 20:04:08 -0300

    #25 feat: add FuncionarioOutputDTO

---
+++ backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/dto/FuncionarioOutputDTO.kt
@@ -0,0 +1,19 @@
+package br.ufpr.dac.funcionario_service.resource.dto
+
+import br.ufpr.dac.funcionario_service.domain.Funcionario
+
+data class FuncionarioOutputDTO(
+    val codigo: Long,
+    val cpf: String,
+    val nome: String,
+    val email: String,
+    val telefone: String
+) {
+    constructor(funcionario: Funcionario) : this(
+        codigo = funcionario.codigo,
+        cpf = funcionario.cpf,
+        nome = funcionario.nome,
+        email = funcionario.email,
+        telefone = funcionario.telefone
+    )
+}
