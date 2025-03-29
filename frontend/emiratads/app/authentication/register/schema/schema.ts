db6551718f82c04a16e435bd6ea06ded70a9decc
raul.bana <raulfbana@gmail.com>
2025-03-29 19:35:15 -0300

    feat: authentication interfaces

--- frontend/emiratads/app/authentication/register/schema/schema.ts
+++ frontend/emiratads/app/authentication/register/schema/schema.ts
@@ -1,19 +1,46 @@
 import { validateCPF } from '@/app/utils/cpfValidator';
-import {z} from 'zod';
+import { z } from 'zod';
 
 export const RegisterSchema = z.object({
-  cpf: z.string().nonempty().min(14).max(14).refine((cpf) => validateCPF(cpf), {
-    message: 'CPF inválido',
- }),
-    email: z.string().email(),
-    nome: z.string().nonempty(),
-    endereco: z.object({
-        cep: z.string().nonempty().min(9).max(9),
-        uf: z.string().nonempty(),
-        cidade: z.string().nonempty(),
-        bairro: z.string().nonempty(),
-        rua: z.string().nonempty(),
-        numero: z.string().nonempty(),
-        complemento: z.string().optional(),
+  cpf: z
+    .string()
+    .nonempty({ message: 'O CPF é obrigatório' })
+    .min(14, { message: 'O CPF deve ter 14 caracteres (incluindo pontuação)' })
+    .max(14, { message: 'O CPF deve ter no máximo 14 caracteres' })
+    .refine((cpf) => validateCPF(cpf), {
+      message: 'CPF inválido',
     }),
+  email: z
+    .string()
+    .nonempty({ message: 'O e-mail é obrigatório' })
+    .email({ message: 'O e-mail deve ser válido' }),
+  nome: z
+    .string()
+    .nonempty({ message: 'O nome é obrigatório' }),
+  endereco: z.object({
+    cep: z
+      .string()
+      .nonempty({ message: 'O CEP é obrigatório' })
+      .min(9, { message: 'O CEP deve ter 9 caracteres (incluindo o traço)' })
+      .max(9, { message: 'O CEP deve ter no máximo 9 caracteres' }),
+    uf: z
+      .string()
+      .nonempty({ message: 'O estado (UF) é obrigatório' }),
+    cidade: z
+      .string()
+      .nonempty({ message: 'A cidade é obrigatória' }),
+    bairro: z
+      .string()
+      .nonempty({ message: 'O bairro é obrigatório' }),
+    rua: z
+      .string()
+      .nonempty({ message: 'A rua é obrigatória' }),
+    numero: z
+      .string()
+      .nonempty({ message: 'O número é obrigatório' }),
+    complemento: z
+      .string()
+      .max(100, { message: 'O complemento deve ter no máximo 100 caracteres' })
+      .optional(),
+  }),
 });
