181969cfbee981ca3c629c15e07ce34e326bc40d
AlissonGSantos <alisson.gabriel@ufpr.br>
2025-04-01 00:12:54 -0300

    #35 feat: Create voo tables

--- backend/database/sql/02-create-tables.sql
+++ backend/database/sql/02-create-tables.sql
@@ -7,7 +7,7 @@ CREATE SEQUENCE emiratads_funcionario.funcionario_seq
 	NO CYCLE;
 
 CREATE TABLE emiratads_funcionario.funcionario (
-    codigo serial4 NOT NULL,
+    codigo bigserial NOT NULL,
     cpf varchar(11) NOT NULL,
     nome varchar(50) NOT NULL,
     email varchar(30) NOT NULL,
@@ -33,7 +33,7 @@ CREATE SEQUENCE emiratads_cliente.endereco_seq
 	NO CYCLE;
 
 CREATE TABLE emiratads_cliente.endereco (
-    codigo serial4 NOT NULL,
+    codigo bigserial NOT NULL,
     cep varchar(8) NOT NULL,
     uf varchar(2) NOT NULL,
     cidade varchar(30) NOT NULL,
@@ -46,14 +46,46 @@ CREATE TABLE emiratads_cliente.endereco (
 );
 
 CREATE TABLE emiratads_cliente.cliente (
-    codigo serial4 NOT NULL,
+    codigo bigserial NOT NULL,
     cpf varchar(11) NOT NULL,
     nome varchar(50) NOT NULL,
     email varchar(30) NOT NULL,
     saldo_milhas numeric(10, 2) NOT NULL,
-    endereco_codigo int4 NOT NULL,
+    endereco_codigo bigint NOT NULL,
     ativo boolean NOT NULL DEFAULT true,
     CONSTRAINT cliente_pkey PRIMARY KEY (codigo),
     CONSTRAINT cliente_unique_cpf UNIQUE (cpf),
     CONSTRAINT cliente_endereco_fk FOREIGN KEY (endereco_codigo) REFERENCES emiratads_cliente.endereco(codigo)
+);
+
+CREATE TABLE emiratads_voo.aeroporto (
+    codigo char(3) NOT NULL,
+    nome varchar(50) NOT NULL,
+    cidade varchar(30) NOT NULL,
+    uf varchar(2) NOT NULL,
+    ativo boolean NOT NULL DEFAULT true,
+    CONSTRAINT aeroporto_pkey PRIMARY KEY (codigo)
+);
+
+CREATE TABLE emiratads_voo.estado_voo (
+    codigo SERIAL PRIMARY KEY,
+    sigla CHAR(3) NOT NULL,
+    descricao VARCHAR(20) NOT NULL
+);
+
+-- Atualização da tabela voo para incluir o campo estado_codigo
+CREATE TABLE emiratads_voo.voo (
+    codigo varchar(8) NOT NULL,
+    data timestamptz NOT NULL,
+    valor_passagem numeric(10, 2) NOT NULL,
+    quantidade_poltronas_total smallint NOT NULL,
+    quantidade_poltronas_ocupadas smallint NOT NULL default 0,
+    estado_codigo integer NOT NULL,
+    aeroporto_origem char(3) NOT NULL,
+    aeroporto_destino char(3) NOT NULL,
+    ativo boolean NOT NULL DEFAULT true,
+    CONSTRAINT voo_pkey PRIMARY KEY (codigo),
+    CONSTRAINT voo_estado_fk FOREIGN KEY (estado_codigo) REFERENCES emiratads_voo.estado_voo(codigo),
+    CONSTRAINT voo_aeroporto_origem_fk FOREIGN KEY (aeroporto_origem) REFERENCES emiratads_voo.aeroporto(codigo),
+    CONSTRAINT voo_aeroporto_destino_fk FOREIGN KEY (aeroporto_destino) REFERENCES emiratads_voo.aeroporto(codigo)
 );
