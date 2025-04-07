6955efd9a1d095d2943ce2e6b8ab699785cdfef6
salgado2004 <leosalgado2004@gmail.com>
2025-04-07 12:06:58 -0300

    #54 feat: create endpoints for 'comprar milha' e 'consultar extrato'

---
+++ backend/cliente-service/src/main/kotlin/br/ufpr/dac/cliente_service/resource/MilhasController.kt
@@ -0,0 +1,29 @@
+package br.ufpr.dac.cliente_service.resource
+
+import br.ufpr.dac.cliente_service.resource.dto.ExtratoDTO
+import br.ufpr.dac.cliente_service.resource.dto.MilhasCompraDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import utils.dto.ClienteOutputDTO
+
+@RestController
+@RequestMapping("/clientes/{codigo}/milhas")
+class MilhasController(private val service: MilhasService) {
+
+    @PutMapping
+    fun comprarMilhas(@PathVariable codigo: Long, @RequestBody milhas: MilhasCompraDTO): ResponseEntity<ClienteOutputDTO> {
+        val response = service.comprarMilhas(codigo, milhas)
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping
+    fun emitirExtrato(@PathVariable codigo: Long): ResponseEntity<ExtratoDTO> {
+        val extrato = service.emitirExtrato(codigo)
+        return ResponseEntity.ok(extrato)
+    }
+}
