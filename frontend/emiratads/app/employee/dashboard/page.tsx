427fe115c6696989dffb410ebfa4b9a3d2565d63
raul.bana <raulfbana@gmail.com>
2025-04-09 00:07:33 -0300

    refactor: move employee dashboar directory

---
+++ frontend/emiratads/app/employee/dashboard/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import DataTable from "@/app/components/DataTable/DataTable";
+
+const EmployeeDashboard = () => {
+  return (
+    <div>
+      <DataTable
+        data={[]}
+        controls={[]}
+        columns={[]}
+      />
+    </div>
+  );
+};
+
+export default EmployeeDashboard;
