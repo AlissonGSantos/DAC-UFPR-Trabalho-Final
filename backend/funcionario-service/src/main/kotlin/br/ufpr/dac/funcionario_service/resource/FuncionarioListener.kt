b0c737723ea8c9ae2ef5b20cf16387c8bf762dcf
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-04-14 23:25:03 -0300

    #71 wip: add cadastrarFuncionarioSaga method to FuncionarioListener

--- backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/FuncionarioListener.kt
+++ backend/funcionario-service/src/main/kotlin/br/ufpr/dac/funcionario_service/resource/FuncionarioListener.kt
@@ -1,9 +1,34 @@
 package br.ufpr.dac.funcionario_service.resource
 
 import com.google.gson.Gson
+import jakarta.validation.ConstraintViolation
+import jakarta.validation.Validation
+import jakarta.validation.Validator
+import jakarta.validation.ValidatorFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import utils.dto.FuncionarioOutputDTO
+import utils.dto.RabbitMessageDTO
 
 class FuncionarioListener(private val service: FuncionarioService) {
+    private val gson = Gson()
+    private val factory: ValidatorFactory = Validation.buildDefaultValidatorFactory()
+    private val validator: Validator = factory.validator
+
+    @RabbitListener(queues = ["emiratads.cadastroFuncionario.funcionario"])
+    fun cadastrarFuncionarioSaga(obj: String): String {
+        val response: RabbitMessageDTO<FuncionarioOutputDTO>
+        val funcionarioInput = gson.fromJson(obj, FuncionarioInputDTO::class.java)
+        val violations: Set<ConstraintViolation<FuncionarioInputDTO>> = validator.validate(funcionarioInput)
+
+        if(violations.isEmpty()) {
+            val funcionario = service.saveFuncionario(funcionarioInput)
+            response = RabbitMessageDTO(true, funcionario)
+        } else {
+            response = RabbitMessageDTO(false, "Funcionário informado com formato inválido: ${violations}")
+        }
+
+        return gson.toJson(response)
+    }
 
     @RabbitListener(queues = ["emiratads.login.funcionario"])
     fun dadosLoginFuncionario(code: String): String {
