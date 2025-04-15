478d59918836c6f359a0cfabf64465b58fe30a91
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-04-14 23:35:46 -0300

    #71 wip: add cadastroFuncionario queue to AutenticacaoListener

--- backend/autenticacao-service/src/main/kotlin/br/ufpr/dac/autenticacao_service/resource/AutenticacaoListener.kt
+++ backend/autenticacao-service/src/main/kotlin/br/ufpr/dac/autenticacao_service/resource/AutenticacaoListener.kt
@@ -10,7 +10,7 @@ import utils.dto.UsuarioInputDTO
 @Service
 class AutenticacaoListener(private val authService: AuthService) {
 
-    @RabbitListener(queues = ["emiratads.autocadastro.autenticacao"])
+    @RabbitListener(queues = ["emiratads.autocadastro.autenticacao", "emiratads.cadastroFuncionario.auth"])
     fun autocadastroSaga(obj: String): String {
         val gson = Gson()
 
