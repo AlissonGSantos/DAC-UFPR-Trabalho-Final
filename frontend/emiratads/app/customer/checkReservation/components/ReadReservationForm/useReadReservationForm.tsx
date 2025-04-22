ca251406557276203997c2ebb3a8e74d805ec658
Pedro-H108 <pedro.souza.1008@gmail.com>
2025-04-21 22:27:08 -0300

    #76 feat:frontend:lógica para os requisitos

--- frontend/emiratads/app/customer/checkReservation/components/ReadReservationForm/useReadReservationForm.tsx
+++ frontend/emiratads/app/customer/checkReservation/components/ReadReservationForm/useReadReservationForm.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { ReadReservationSchema } from "../../schema/schema";
-import { z } from "zod";
+import { date, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 
@@ -11,6 +11,7 @@ type ReadReservationFormData = z.infer<typeof ReadReservationSchema>;
 
 const useReadReservationForm = () => {
   const [showSuccess, setShowSuccess] = useState(false);
+  const [canCheckIn, setCanCheckIn] = useState(false);
   const {
     register,
     handleSubmit,
@@ -20,16 +21,15 @@ const useReadReservationForm = () => {
   } = useForm<ReadReservationFormData>({
     resolver: zodResolver(ReadReservationSchema),
     defaultValues: {
-        CodeReservation: "",
-        dateTimeFlight: "",
-        OriginAirport: "",
-        DestinationAirport: "",
-        ticketValue: "",
-        miles: "",
-        flightStatus: ""
+      CodeReservation: "",
+      dateTimeFlight: "",
+      OriginAirport: "",
+      DestinationAirport: "",
+      ticketValue: "",
+      miles: "",
+      flightStatus: "",
     },
   });
-
 
   const onSubmit = (data: ReadReservationFormData) => {
     console.log("Form", data);
@@ -38,9 +38,17 @@ const useReadReservationForm = () => {
     setValue("OriginAirport", "");
     setValue("DestinationAirport", "");
     setValue("ticketValue", "");
-    setValue("miles","");
+    setValue("miles", "");
     setValue("flightStatus", "");
-    setShowSuccess(true); 
+    setShowSuccess(true);
+  };
+
+  const verifyDateCheckIn = (bookingDate: string) => {
+    const today = new Date();
+    const flightDate = new Date(bookingDate);
+    const difTime = Math.abs(flightDate.getTime() - today.getTime());
+    const difHours = Math.ceil(difTime / (1000 * 3600));
+    return difHours < 48;
   };
 
   return {
