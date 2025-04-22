c90dddbad97633f98b827c2772b69d750bd8cf38
Pedro-H108 <pedro.souza.1008@gmail.com>
2025-04-21 22:53:39 -0300

    #76 feat:frontend: implementação da lógica da tela

--- frontend/emiratads/app/customer/checkReservation/components/ReadReservationForm/useReadReservationForm.tsx
+++ frontend/emiratads/app/customer/checkReservation/components/ReadReservationForm/useReadReservationForm.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { ReadReservationSchema } from "../../schema/schema";
-import { date, z } from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 
