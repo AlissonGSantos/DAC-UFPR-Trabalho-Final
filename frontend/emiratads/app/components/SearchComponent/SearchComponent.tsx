0cd8d2732b25ad3b625189ab6e819bccc48d72e9
AlissonGSantos <alisson.gabriel@ufpr.br>
2025-05-04 22:54:45 -0300

    #94 style: change search component style

--- frontend/emiratads/app/components/SearchComponent/SearchComponent.tsx
+++ frontend/emiratads/app/components/SearchComponent/SearchComponent.tsx
@@ -20,9 +20,9 @@ const SearchComponent: React.FC<SearchComponentProps> = ({ fromHome }) => {
 
   return (
     <>
-      <div className="flex flex-col h-full w-full bg-slate-800 border-1 border-indigo-800 rounded-lg p-4">
-        <div className="flex flex-col p-4 rounded-lg  gap-6 mb-8">
-          <h1 className="text-3xl font-bold text-slate-300">Buscar Voos</h1>
+      <div className="flex flex-col h-full w-full bg-slate-800 border-1 border-indigo-800 rounded-lg px-2">
+        <div className="flex flex-col p-4 rounded-lg gap-4">
+          <h1 className="text-2xl font-bold text-slate-300">Buscar Voos</h1>
           <SearchForm
             onChangeDestination={onChangeDestination}
             onChangeOrigin={onChangeOrigin}
