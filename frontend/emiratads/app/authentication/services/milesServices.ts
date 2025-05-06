0d8cea1c2d2400027d1314582ffe76d1a35d1ef3
salgado2004 <leosalgado2004@gmail.com>
2025-05-06 17:43:10 -0300

    feat: integrate mileage purchase service in frontend

--- frontend/emiratads/app/authentication/services/milesServices.ts
+++ frontend/emiratads/app/authentication/services/milesServices.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "@/app/types/InvoiceTypes";
+import { Client } from "@/app/types/AuthTypes";
 import axios from "axios";
 
 const milesServices = {
@@ -6,6 +7,12 @@ const milesServices = {
         const res = await axios.get<Invoice>(`http://localhost:8082/clientes/${clientId}/milhas`);
         const data = res.data;
         return data;
+    },
+    
+    buyMiles: async (clientId: string, data: { quantidade: number }): Promise<Client> => {
+        const res = await axios.put<Client>(`http://localhost:8082/clientes/${clientId}/milhas`, data);
+        const client = res.data;
+        return client;
     }
 }
 
