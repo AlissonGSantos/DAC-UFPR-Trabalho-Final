7e8f1f752dc6cf73887f8ecfeb8fc33589e6ddea
salgado2004 <leosalgado2004@gmail.com>
2025-05-15 09:48:41 -0300

    feat: implement cancellation of reserved seats and update reservation state handling

--- backend/reserva-service/src/main/kotlin/br/ufpr/dac/reserva_service/repository/IPoltronaRepository.kt
+++ backend/reserva-service/src/main/kotlin/br/ufpr/dac/reserva_service/repository/IPoltronaRepository.kt
@@ -2,10 +2,13 @@ package br.ufpr.dac.reserva_service.repository
 
 import br.ufpr.dac.reserva_service.domain.PoltronasReservadas
 import br.ufpr.dac.reserva_service.domain.embeddable.PoltronasReservadasId
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+
 
 @Repository
 interface IPoltronaRepository : JpaRepository<PoltronasReservadas, PoltronasReservadasId> {
@@ -18,4 +21,9 @@ interface IPoltronaRepository : JpaRepository<PoltronasReservadas, PoltronasRese
        """
     )
     fun getPoltronasReservadas(@Param("codigo_voo") codigo_voo: String): List<PoltronasReservadas>
+
+    @Modifying
+    @Transactional
+    @Query("delete from PoltronasReservadas p where p.codigo_reserva = :codigo_reserva")
+    fun deletePoltronasCanceladas(@Param("codigo_reserva") codigo_reserva: String)
 }
