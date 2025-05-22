cb4f0c793eaf3029cae49f5adf6a930e217dce37
AlissonGSantos <alisson.gabriel@ufpr.br>
2025-05-21 21:57:48 -0300

    feat: add warning on login page

--- frontend/emiratads/app/authentication/login/page.tsx
+++ frontend/emiratads/app/authentication/login/page.tsx
@@ -1,16 +1,32 @@
+"use client";
 import { robotoFont } from "@/app/assets/fontsSetup";
 import React from "react";
 import LoginForm from "./components/LoginForm/LoginForm";
 import Logo from "@/app/assets/images/logos/emiratadsLogo.png";
 import Image from "next/image";
+import { Info } from "phosphor-react";
 
 const Login = () => {
   return (
     <div className="w-full">
       <div className="flex justify-center flex-col mx-auto w-1/2">
-        <div
-          className={`my-8 w-full text-3xl ${robotoFont.className} text-indigo-900`}
-        ></div>
+        <div className={`my-8 w-full text-3xl ${robotoFont.className} text-indigo-900`} >
+          
+          <div className="mt-6 p-4 bg-red-900/20 border border-red-700 rounded-lg mx-8">
+              <div className="flex items-center mb-2">
+                <Info size={24} className="text-red-500 mr-2" />
+                <span className="text-slate-300 text-sm">
+                  Atenção!
+                </span>
+              </div>
+              <p className="text-center text-sm text-slate-300">
+                Este site foi desenvolvido exclusivamente para fins acadêmicos e não possui fins comerciais. 
+                Não realiza transações financeiras nem coleta de dados sensíveis. 
+                Todos os dados registrados são fictícios e serão excluídos posteriormente.
+              </p>
+            </div>
+        </div>
+        
         <div
           className={`flex flex-col bg-indigo min-w-2xl w-full-50 w-full rounded-md p-4 border-2 border-indigo-900`}
         >
@@ -26,7 +42,7 @@ const Login = () => {
             LOGIN
           </h1>
           <LoginForm></LoginForm>
-          <span className="text-center font-bold text-slate-300 text-sm mx-32 my-8">
+          <span className="text-center font-bold text-slate-300 text-sm mx-32 mt-4">
             Não possui conta?{" "}
             <a
               href="/authentication/register"
@@ -34,7 +50,7 @@ const Login = () => {
             >
               Cadastre-se
             </a>
-          </span>
+          </span>  
         </div>
       </div>
     </div>
